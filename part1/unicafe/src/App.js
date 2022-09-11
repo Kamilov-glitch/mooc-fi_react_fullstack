17a76e1be5bafb0b236431db9aa2d76eb720c4b6
Fuad Kamilov <fuad.kamilov@scandiweb.com>
2022-09-11 14:37:34 +0400

    Finish unicafe project

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,14 +13,34 @@ const Statistics = (props) => {
     return "No feedback given"
   }
   return (
-    <div>
-      <StatisticLine text="good" value={props.good}/>
-      <StatisticLine text="neutral" value={props.neutral}/>
-      <StatisticLine text="bad" value={props.bad}/>
-      <StatisticLine text="all" value={props.all}/>
-      <StatisticLine text="average" value={props.average}/>
-      <StatisticLine text="positive" value={props.positive}/>
-    </div>
+    <table>
+      <tbody>
+        <tr>
+          <td>good</td>
+          <td>{props.good}</td>
+        </tr>
+        <tr>
+          <td>neutral</td>
+          <td>{props.neutral}</td>
+        </tr>
+        <tr>
+          <td>bad</td>
+          <td>{props.bad}</td>
+        </tr>
+        <tr>
+          <td>all</td>
+          <td>{props.all}</td>
+        </tr>
+        <tr>
+          <td>average</td>
+          <td>{props.average}</td>
+        </tr>
+        <tr>
+          <td>positive</td>
+          <td>{props.positive}</td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
